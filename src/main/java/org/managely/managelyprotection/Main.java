7723bbeb31426a593e89874a11b40eeb8b55624c
skrekhere <liam@tannock.net>
2021-07-09 14:30:13 -0700

    yo

---
+++ src/main/java/org/managely/managelyprotection/Main.java
@@ -0,0 +1,10 @@
+package org.managely.managelyprotection;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable(){
+
+    }
+}
